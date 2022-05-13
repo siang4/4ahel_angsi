6998820d05d39ae98c5e648c2346cd676e915d73
4ahel-pat_ede <pedelmann@tsn.at>
2022-05-13 08:59:45 +0200

    changed by patrick

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #AAP10_Angerer
-
-print("Hallo HTL")
-print("Hallo Patrick, ich bin Simon!")
+#Changed by Patrick
+#FIR-Filter Programm
+from time import time
+pow = 3023
+mod = 12191769932823784859
+seed = time()
+def random() -> int:
+    global seed
+    seed = (1 + (seed * pow)) % mod
+    return seed
+def mac(coeff, values):
+    return sum([coeff[i] * values[i] for i in range(len(coeff))])
+coeff  = [1/256 for x in range(256)]
+values = [0     for x in range(256)]
+for i in range(2048):
+    values[1:] = values[:-1]
+    values[0] = random() % 100
+    print(" " * int(mac(coeff, values)), "*", sep='')
